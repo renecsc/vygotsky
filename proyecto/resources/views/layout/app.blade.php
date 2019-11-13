249d7e5859ca5e2efedd4ceff397e50967e948be
miguel <jorquera1401@gmail.com>
2019-11-13 18:54:04 -0300

    Cambio de apariencia pagina principal
    Se incluye las noticias de facebook, para tener mas centralizado,
    Se  agregaron los links importantes

--- proyecto/resources/views/layout/app.blade.php
+++ proyecto/resources/views/layout/app.blade.php
@@ -17,10 +17,12 @@
     </head>
 
     <body>
+    <div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v5.0&appId=576015375838054&autoLogAppEvents=1"></script>
         <!-- Cabecera -->
         <header id="header" >  
              
-                <nav id="navbar" class="navbar navbar-expand-lg fixed-top bg-light navbar-light">
+                <nav id="navbar" class="navbar navbar-expand-lg fixed-top bg-light border  navbar-light">
                     <div id="cabecera" class="container">                        
                         <img class="navbar-brand logo"   src="logo.png" href="/home" alt="">
                         <a class="navbar-brand" href="/home"><p class="titulo">Colegio Vygotsky</p></a>
@@ -51,12 +53,11 @@
         <!--<div class="espacio"></div>-->
         <main role="main" class="container col home" >
            @yield("contenido")
-           
+            @yield("media")   
         </main>
         <!-- Footer -->
 
 
-        <!-- Footer -->
         <footer class="page-footer font-small pt-4">
 
             <!-- Footer Links -->
@@ -78,18 +79,73 @@
         
                 <!-- Grid column -->
                 <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+                <h6 class="text-uppercase mb-4 font-weight-bold">Menu</h6>
                 <p>
-                    <a href="#!">MDBootstrap</a>
+                    <a href="https://www.sistemadeadmisionescolar.cl/" target="_blank">Admisión Escolar</a>
                 </p>
                 <p>
-                    <a href="#!">MDWordPress</a>
+                    <a href="#!">Galería de Imágenes</a>
                 </p>
                 <p>
-                    <a href="#!">BrandFlow</a>
+                    <a href="javascript:openit(1)" >Guía de Padres para Ciberbullyng</a>
+                    <div id="anuncio" style="right:30%;top:15%;align-text:center; position: fixed; z-index:100;text-align:center;visibility:hidden"> 
+                            <div align="center" style="margin-bottom:-30px;"> 
+                            <a href="javascript:closeit(1)"><font face="Courier" size="1">#CERRAR</font></a> 
+                            </div> 
+                            <br /> 
+                            <iframe width="480" height="320"
+                                src="https://www.youtube.com/embed/0r3owGHbUJ8?controls=1">
+                                </iframe> 
+                            </div> 
+
+                            <div class="clear"> 
+                    </div>
+                    
                 </p>
                 <p>
-                    <a href="#!">Bootstrap Angular</a>
+                    <a href="javascript:openit(2)">Plan Familia Preparada</a>
+                    <div id="anuncio_familia" style="right:30%;top:15%; position: fixed; z-index:100;text-align:center;visibility:hidden"> 
+                            <div align="center" style="margin-bottom:-30px;"> 
+                            <a href="javascript:closeit(2)"><font face="Courier" size="1">#CERRAR</font></a>
+                            
+                            </div> 
+                            <br /> 
+                            <iframe width="480" height="320"
+                                src="https://www.youtube.com/embed/jtYB3HFaEvc?controls=1">
+                                </iframe> 
+                            </div> 
+
+                            <div class="clear"> 
+                    </div>
+                    <script type="text/javascript"> 
+                        //<![CDATA[ 
+
+                        anuncio = document.getElementById("anuncio");
+                        familia = document.getElementById("anuncio_familia");
+                        function closeit(opcion){
+                            if(opcion==1){
+                                anuncio.style.visibility = "hidden";
+                            }
+                            if(opcion==2){
+                                familia.style.visibility= "hidden";
+                            } 
+                         
+                        } 
+                        function openit(opcion){
+                            if(opcion==1){
+                                closeit(2);
+                                anuncio.style.visibility = "visible";
+                            
+                            }
+                            if(opcion==2){
+                                closeit(1);
+                                familia.style.visibility="visible";
+                                
+                            }
+                        }
+                        //]]> 
+                    </script>
+                
                 </p>
                 </div>
                 <!-- Grid column -->
@@ -98,26 +154,23 @@
         
                 <!-- Grid column -->
                 <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+                <h6 class="text-uppercase mb-4 font-weight-bold">Link de Interés</h6>
                 <p>
-                    <a href="#!">Your Account</a>
+                    <a href="http://fullcollege.net/consulta/" target="_blank">Acceso a FullCollege</a>
                 </p>
                 <p>
-                    <a href="#!">Become an Affiliate</a>
+                    <a href="http://www.simce.cl/ficha2017/?lista=1&rbd=16509&establecimiento=COLEGIO+CENTRO+EDUCACIONAL+VYGOTSKY&region=0&comuna=0" target="_blank">Resultados Simce 2017 </a>
                 </p>
                 <p>
-                    <a href="#!">Shipping Rates</a>
-                </p>
-                <p>
-                    <a href="#!">Help</a>
-                </p>
+                    <a href="#!">Inicio de ACLES y Reforzamientos</a>
+                </p>>
                 </div>
         
                 <!-- Grid column -->
                 <hr class="w-100 clearfix d-md-none">
         
                 <!-- Grid column -->
-                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <div class="col-md-3 col-lg-3 col-xl-5 mx-auto mt-3">
                 <h6 class="text-uppercase mb-4 font-weight-bold">Contacto</h6>
                 <p>
                     <i class=" fa fa-home mr-3"></i>Camino a Zapallar Km. 0.65, Curicó</p>
@@ -127,12 +180,12 @@
                     <i class="fa fa-phone mr-3"></i> (75)-2322216 - 2336765 - (9)84759648</p>
                 </div>
                 <!-- Grid column -->
-        
+         
             </div>
             <!-- Footer links -->
         
             <hr>
-        
+            
             <!-- Grid row -->
             <div class="row d-flex align-items-center">
         
@@ -156,7 +209,7 @@
                 <div class="text-center text-md-right">
                     <ul class="list-unstyled list-inline">
                     <li class="list-inline-item">
-                        <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                        <a class="btn-floating btn-sm rgba-white-slight mx-1" href="https://www.facebook.com/colegio.vygotsky" target="_blank">
                         <i class="fab fa fa-facebook"></i>
                         </a>
                     </li>

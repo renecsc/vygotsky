bb0391b49c20057f648d53be4d9ac68de0b4f4d2
miguel <jorquera1401@gmail.com>
2019-11-19 12:52:14 -0300

    Proyecto Educativo
    se agregan datos a la pagina proyecto educativo

--- proyecto/resources/views/layout/app.blade.php
+++ proyecto/resources/views/layout/app.blade.php
@@ -14,6 +14,7 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="shortcut icon" href="logo.png"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     </head>
 
     <body>
